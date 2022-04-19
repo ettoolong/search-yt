d9484ae6d0b26142b01d56aa804f5d6457c428d9
ettoolong <ettoolong@hotmail.com>
2022-04-19 10:46:20 +0800

    add search box

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import React, { useState, useRef } from 'react';
+
+import SearchService from './services/search.service';
 import './App.css';
 
 function App() {
+  const inputRef = useRef()
+  const [searchResult, setSearchResult] = useState([])
+
+  function search() {
+    const searchTerm = inputRef.current.value.trim()
+    if (searchTerm !== '') {
+      SearchService.getSearchResult(searchTerm).then(result => {
+        setSearchResult(result)
+      });
+    }
+  }
+
   return (
     <div className="App">
+      <div className="searchBox">
+        <input
+          ref={inputRef}
+          className="input"
+          onKeyUp={e => {
+            if (e.key === 'Enter') {
+              search();
+            }
+          }}
+        >
+        </input>
+        <button className="button" onClick={search}>Search</button>
+      </div>
     </div>
   );
 }
